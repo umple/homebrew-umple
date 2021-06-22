d3be092cd67898d6ebe288c9909067978dd1c4bf
Fred Lafleche <31863167+fred-lafleche@users.noreply.github.com>
2021-06-22 15:25:39 -0400

    Update umple.rb
    Changed the URL so it matches the latest version of umple. The sha256 was generated by homebrew. Changed the test block so we create a simple ump file and compile it. Then, we verify if the java file and the class file are created.

--- Formula/umple.rb
+++ Formula/umple.rb
@@ -1,9 +1,9 @@
 class Umple < Formula
   desc "Umple: Model-Oriented Programming - embed models in code and vice versa and generate complete systems"
   homepage "http://www.umple.org"
-  url "https://github.com/umple/umple/archive/v.1.27.1.tar.gz"
-  sha256 "548ea671f9e5d2e9f7972c20d6527774fe3a466a2e65f5c8c4809c1fa6a0ad93"
-  depends_on :java => "1.8+"
+  url "https://github.com/umple/umple/archive/refs/tags/v1.30.2.tar.gz"
+  sha256 "fc2a880c876fdebc00657e2d4461ad5237bd85bda85742778b2ec49c143c31d4"
+  license "MIT"
   depends_on "ant" => "with-ivy"
   depends_on "ant-contrib"
   def install
@@ -15,6 +15,12 @@ class Umple < Formula
     libexec.install Dir["dist/umple-*.jar"]
   end
   test do
-    system "dev-tools/tumple"
+    cmd =
+    "echo 'class X{ a; }' >test.ump;
+    umple test.ump -c -;
+    ls *java;"
+    `#{cmd}`
+    assert_equal(true, File.file?("X.java"), "Java file was not created at umple compilation")
+    assert_equal(true, File.file?("X.class"), "Java file was not compiled at umple compilation")
   end
 end
