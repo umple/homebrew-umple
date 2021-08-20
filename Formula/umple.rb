e3b0fe1fda860da07c48db6fe2970f0791d9d94b
TCLethbridge <timothyclethbridge@gmail.com>
2021-08-20 19:57:58 -0400

    Updating umple.rb here to keep in sync
    Need to keep in sync with https://github.com/Homebrew/homebrew-core/blob/master/Formula/umple.rb

--- Formula/umple.rb
+++ Formula/umple.rb
@@ -1,21 +1,19 @@
 class Umple < Formula
   desc "Modeling tool/programming language that enables Model-Oriented Programming"
   homepage "https://www.umple.org"
-  url "https://github.com/umple/umple/releases/download/v1.30.2/umple-1.30.2.5248.dba0a5744.jar"
-  version "1.30.2.5248.dba0a5744"
-  sha256 "11523494f9f4f360cce9226ed7f68641aa733fa13d4009f871060f9fe54c6a52"
+  url "https://github.com/umple/umple/releases/download/v1.31.1/umple-1.31.1.5860.78bb27cc6.jar"
+  version "1.31.1.5860.78bb27cc6"
+  sha256 "686beb3c8aa3c0546f4a218dad353f4efce05aed056c59ccf3d5394747c0e13d"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bd41188baf2192be2d75e098a45efa7a7d83e7c8a76daad5b059bdff6f859071"
-    sha256 cellar: :any_skip_relocation, big_sur:       "bd41188baf2192be2d75e098a45efa7a7d83e7c8a76daad5b059bdff6f859071"
-    sha256 cellar: :any_skip_relocation, catalina:      "bd41188baf2192be2d75e098a45efa7a7d83e7c8a76daad5b059bdff6f859071"
-    sha256 cellar: :any_skip_relocation, mojave:        "bd41188baf2192be2d75e098a45efa7a7d83e7c8a76daad5b059bdff6f859071"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c80a927eb6c67b19974ded86bca1daca1e2ad6d41e6e0a5c56b8f2e409b89730"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "b588ed54d528a7301c1450bd03a8651abcb88119e18dd9166631adf3143a2180"
+    sha256 cellar: :any_skip_relocation, big_sur:       "b588ed54d528a7301c1450bd03a8651abcb88119e18dd9166631adf3143a2180"
+    sha256 cellar: :any_skip_relocation, catalina:      "b588ed54d528a7301c1450bd03a8651abcb88119e18dd9166631adf3143a2180"
+    sha256 cellar: :any_skip_relocation, mojave:        "b588ed54d528a7301c1450bd03a8651abcb88119e18dd9166631adf3143a2180"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8290ce892b832b7c4fa229b7ad10b0e2b8f78371f3c23c07a07542f2ae4af18b"
   end
 
-  depends_on "ant"
-  depends_on "ant-contrib"
   depends_on "openjdk"
 
   def install
